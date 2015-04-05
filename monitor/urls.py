5579e3068c4662b3f69371d6bc3298ecedad9acc
ariarezaei <aria.rezaii@gmail.com>
2015-04-05 16:22:54 +0430

    With charts in the dashboard

--- monitor/urls.py
+++ monitor/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 
 from monitor import views
 
-urlpatterns = patterns('shop.views',
+urlpatterns = patterns('monitor.views',
                        url(r'^$', views.index))
