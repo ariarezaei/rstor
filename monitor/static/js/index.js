a726f223291a8a915d3a2efdd80895d3ce010193
ariarezaei <aria.rezaii@gmail.com>
2015-04-21 18:14:58 +0430

    Online charts are ready now (Aria)

--- monitor/static/js/index.js
+++ monitor/static/js/index.js
@@ -1,32 +1,107 @@
+var graphsInterval;
+
+var hitRatioGraph;
+var throughputGraph;
+var responseGraph;
+var requestGraph;
+
+hitRatioData = [];
+throughputData = [];
+responseData = [];
+requestData = [];
+
+var globalTime;
+
 $(function() {
-    console.log("Started this");
-    ids = [
-        'wr-ratio',
-        'wr-throughput',
-        'wr-response',
-        'wr-request'
-    ];
+    console.log("Initiated the charts");
 
-    modes = [
-        'Hit Ratio',
-        'Throughput',
-        'Mean Response Time',
-        'Mean Number of Requests'
-    ];
+    hitRatioGraph = start_graph("wr-ratio", "Hit Ratio");
+    throughputGraph = start_graph("wr-throughput", "Throughput");
+    responseGraph = start_graph("wr-response", "Mean Response Time");
+    requestGraph = start_graph("wr-request", "Mean Number of Requests");
 
-    data = {};
+    graphsInterval = window.setInterval(update_charts, 500);
 
+    globalTime = 1;
+
+});
+
+
+function add_point(first, second, time, graph, data) {
+
+    data.push({
+        'time': time+"s",
+        'wr_res': first,
+        'rd_res': second
+    });
+
+    graph.setData(data);
+}
+
+function update_charts()
+{
+    console.log("Update started");
     $.get("livestat", function(result){
-        alert("DATA IS LOADED!");
+        console.log("Data is loaded, result = ");
         data = result;
         console.log(data);
+        // Hit Ratio. Note: YOU SHOULD FIRST PASS THE 'WRITE' NUMBER THROUGH
+        add_point(
+            data['write_hit_rate'],
+            data['read_hit_rate'],
+            globalTime,
+            hitRatioGraph,
+            hitRatioData
+        );
+
+        // Throughput
+        add_point(
+            data['throughput_write'],
+            data['throughput_read'],
+            globalTime,
+            throughputGraph,
+            throughputData
+        );
+
+        // Response Time
+        add_point(
+            data['write_mean_response'],
+            data['read_mean_response'],
+            globalTime,
+            responseGraph,
+            responseData
+        );
+
+        // Request Number
+        add_point(
+            data['writes'],
+            data['reads'],
+            globalTime,
+            requestGraph,
+            requestData
+        );
+
     }, "json");
 
-    for (i=0;i<4;i++)
-    {
-        draw_graph(ids[i], modes[i], data);
-    }
-});
+    globalTime += 1;
+}
+
+function start_graph(id, mode)
+{
+    return Morris.Line({
+        element: id,
+
+        data: [],
+
+        xkey: 'time',
+        ykeys: ['wr_res', 'rd_res'],
+        labels: ['Write + ' + mode, 'Read ' + mode],
+
+        hideHover: 'auto',
+
+        smooth: 'false'
+    });
+}
 
 function draw_graph(id, mode, data)
 {
