fcb2ba4bf3600bc33c089fe8a76b03a5a3e92566
ariarezaei <aria.rezaii@gmail.com>
2015-05-14 19:21:29 +0430

    First page almost finished (Aria).

--- monitor/static/js/index.js
+++ monitor/static/js/index.js
@@ -1,125 +1,272 @@
-var graphsInterval;
-
-var hitRatioGraph;
-var throughputGraph;
-var responseGraph;
-var requestGraph;
-
-hitRatioData = [];
-throughputData = [];
-responseData = [];
-requestData = [];
-
-var globalTime;
+// The time between two updates
+var T = 5;
+// The number of data points in a chart
+var N = 30;
+// Current update handler
+var updateHandler = undefined;
+var isLoading = false;
+var names = ["wr-ratio", "wr-throughput", "wr-response", "wr-request"];
 
 $(function() {
-    console.log("Initiated the charts");
+    //console.log("Initiated the charts");
 
-    initZero(hitRatioData, 30);
-    initZero(throughputData, 30);
-    initZero(responseData, 30);
-    initZero(requestData, 30);
+    Highcharts.setOptions({
+        lang:{
+            loading: "Please select a cache name."
+        }
+    });
 
-    hitRatioGraph = start_graph("wr-ratio", "Hit Ratio", hitRatioData);
-    throughputGraph = start_graph("wr-throughput", "Throughput", throughputData);
-    responseGraph = start_graph("wr-response", "Mean Response Time", responseData);
-    requestGraph = start_graph("wr-request", "Mean Number of Requests", requestData);
+    create_graph(
+        'wr-ratio',
+        'Read & Write Hit Ratio',
+        'Hit Ratio (%)',
+        'Hit Ratio'
+    );
 
-    graphsInterval = window.setInterval(update_charts, 2000);
+    create_graph(
+        'wr-throughput',
+        'Read & Write Throughput',
+        'Throughput (IO/s)',
+        'Throughput'
+    );
 
-    globalTime = 1;
+    create_graph(
+        'wr-response',
+        'Read & Write Response Time',
+        'Response Time (ms)',
+        'Response Time'
+    );
+
+    create_graph(
+        'wr-request',
+        'Read & Write Mean Request Rate',
+        'Mean Request Rate',
+        'Request Rate'
+    );
+
+    //update_charts();
+    toggleLoading();
+
+    //graphsInterval = window.setInterval(update_charts, T * 1000);
 });
 
-function initZero(data, num)
+
+function toggleLoading()
 {
-    for (var i=0;i<num;i++)
+    if (isLoading == false)
     {
-        data.push({
-            'time': i+"sec",
-            'wr_res': 0,
-            'rd_res': 0
-        })
+        $('[data-type="chart"]').each(function(){
+            $(this).highcharts().showLoading();
+        });
+        isLoading = true;
+    }
+    else
+    {
+        $('[data-type="chart"]').each(function(){
+            $(this).highcharts().hideLoading();
+        });
+        isLoading = false;
     }
 }
 
-
-function add_point(first, second, time, graph, data) {
-    data.push({
-        'time': (time+30)+"sec",
-        'wr_res': first,
-        'rd_res': second
+function turnOffLoading()
+{
+    $('[data-type="chart"]').each(function(){
+        $(this).highcharts().hideLoading();
     });
+    isLoading = false;
+}
 
-    if (data.length > 30)
+function create_graph(id, title, ytitle, mode)
+{
+    $('#' + id).highcharts({
+        chart: {
+            type: 'areaspline'
+        },
+
+        title: {
+            text: title
+        },
+
+        loading: {
+            style: {
+              'font-size': '3em'
+            },
+            labelStyle: {
+                top: '35%'
+            }
+        },
+
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 150
+        },
+
+        yAxis: {
+            title: {
+                text: ytitle
+            },
+
+            min: function(){
+                if (mode == "Hit Ratio")
+                {
+                    return 0;
+                }
+                return undefined;
+            }(),
+            max: function(){
+                if (mode == "Hit Ratio")
+                {
+                    return 100;
+                }
+                return undefined;
+            }(),
+            tickInterval: 10
+        },
+
+        credits: {
+            enabled: false
+        },
+
+        legend: {
+            enabled: false
+        },
+
+        exporting: {
+            enabled: false
+        },
+
+        tooltip: {
+            formatter: function () {
+                return Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                    '<b>' + this.points[0].series.name + '</b>: ' + Highcharts.numberFormat(this.points[0].y, 2) + '<br/>' +
+                    '<b>' + this.points[1].series.name + '</b>: ' + Highcharts.numberFormat(this.points[1].y, 2);
+            },
+            shared: true
+        },
+
+        plotOptions: {
+            spline: {
+                animation: false
+            }
+        },
+
+        series: [{
+            name: 'Read ' + mode
+        }, {
+            name: 'Write' + mode
+        }]
+    });
+}
+
+function init(num)
+{
+    var dataWrite = [],
+        dataRead = [];
+    for (var i=num*(-1);i<0;i++)
     {
-        data.shift();
-    }
+        dataWrite.push({
+            x: (new Date()).getTime() + i * T * 1000,
+            y: 0
+        });
 
-    graph.setData(data);
+        dataRead.push({
+            x: (new Date()).getTime() + i * T * 1000,
+            y: 0
+        });
+    }
+    for (var i=0;i<names.length;i++)
+    {
+        console.log("Initializing " + names[i]);
+        var chart = $('#' + names[i]).highcharts();
+        chart.series[0].setData(dataWrite);
+        chart.series[1].setData(dataRead);
+
+        chart.redraw();
+    }
+}
+
+function load_cache_info(cache_name)
+{
+    $.get("caches/" + cache_name, function(result){
+        console.log("Succesfully acquired cache info");
+        console.log(result);
+        $("#main_disk_name").html(result['main_disk_name']);
+        $("#main_disk_size").html(result['main_disk_size']);
+
+        $("#cache_disk_name").html(result['cache_disk_name']);
+        $("#cache_state").html(result['cache_state']);
+        $("#cache_type").html(result['cache_type']);
+        $("#cache_size").html(result['cache_size']);
+        $("#block_size").html(result['block_size']);
+    }, "json");
+}
+
+function start_updates(interval)
+{
+    //TODO: Make animations smooth, points do not disappear, something shitty happens
+
+    var cache_name = $('#cache-name').val();
+    if (cache_name == "Please choose a cache")  return;
+    load_cache_info(cache_name);
+    // Turning off
+    clearInterval(updateHandler);
+
+    // Stop loading
+    turnOffLoading();
+
+    // Initialize
+    init(N);
+    update_charts(); // This is the first points as user selects a cache
+
+    // Set update intervals
+    updateHandler = window.setInterval(update_charts, T * 1000);
+}
+
+function add_point(write, read, id) {
+    // read comes first
+    // write comes second
+
+    var chart = $('#' + id).highcharts();
+    var curTime = new Date().getTime();
+    chart.series[0].addPoint([curTime, read], false, true);
+    chart.series[1].addPoint([curTime, write], true, true);
 }
 
 function update_charts()
 {
-    console.log("Update started");
+    //console.log("Update started");
     $.get("livestat", function(result){
-        console.log("Data is loaded, result = ");
+        //console.log("Data is loaded, result = ");
         data = result;
-        console.log(data);
+        //console.log(data);
         // Hit Ratio. Note: YOU SHOULD FIRST PASS THE 'WRITE' NUMBER THROUGH
         add_point(
             data['write_hit_rate'],
             data['read_hit_rate'],
-            globalTime,
-            hitRatioGraph,
-            hitRatioData
+            'wr-ratio'
         );
 
         // Throughput
         add_point(
             data['throughput_write'],
             data['throughput_read'],
-            globalTime,
-            throughputGraph,
-            throughputData
+            'wr-throughput'
         );
 
         // Response Time
         add_point(
             data['write_mean_response'],
             data['read_mean_response'],
-            globalTime,
-            responseGraph,
-            responseData
+            'wr-response'
         );
 
         // Request Number
         add_point(
             data['writes'],
             data['reads'],
-            globalTime,
-            requestGraph,
-            requestData
+            'wr-request'
         );
 
     }, "json");
-
-    globalTime += 1;
 }
-
-function start_graph(id, mode, initialData)
-{
-    return Morris.Line({
-        element: id,
-
-        data: initialData,
-
-        xkey: 'time',
-        ykeys: ['wr_res', 'rd_res'],
-        labels: ['Write + ' + mode, 'Read ' + mode],
-
-        hideHover: 'always',
-        axes: 'false',
-        smooth: 'false'
-    });
-}
-
