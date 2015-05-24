fe2385cc2e06c0524dacfda6ba58520d6145cf91
ariarezaei <aria.rezaii@gmail.com>
2015-05-24 14:21:51 +0430

    With database queries (Aria)

--- monitor/urls.py
+++ monitor/urls.py
@@ -5,4 +5,5 @@ from monitor import views
 urlpatterns = patterns('monitor.views',
                        url(r'^$', views.index),
                        url(r'^livestat$', views.instant_stat),
-                       url(r'^dbstat$', views.retrieve_db))
+                       url(r'^dbstat$', views.retrieve_db),
+                       url(r'^caches/(?P<cache_name>\w+)$', views.dummy_cache_config))
--- monitor/views.py
+++ monitor/views.py
@@ -3,29 +3,99 @@ from django.shortcuts import render
 import json
 from django.shortcuts import HttpResponse
 from monitor.models import Log
-from subprocess import call
+from subprocess import *
 import math
 import random
 
+# Main view for index page
 def index(request):
 
     if request.method == u'POST':
         return HttpResponseNotFound("Sorry. We could not find the page you requested. Please check the address and try again.")
 
+    #TODO: find all caches and put it in the array below
+    caches = [
+        'cache1',
+        'cache2',
+        'cache3',
+        'cache_imba',
+        'cache_nakazaki'
+    ]
+
     context = {
-        'title': "Rapid Storage Monitoring Portal"
+        'title': "Rapid Storage Monitoring Portal",
+        'caches': caches
     }
 
     return render(request, 'index.html', context)
 
+# Create some dummy cache configurations for testing purposes
+def dummy_cache_config(request, cache_name):
+    if request.method == u'GET':
+        context = {
+            'main_disk_name': 'Disk1',
+            'main_disk_size': '1024 MB',
+            'cache_disk_name': 'Disk3',
+            'cache_type': 'LRU',
+            'cache_size': '128 MB',
+            'block_size': '4096 KB',
+            'cache_state': '<span class="text-success">NORMAL</span>'
+        }
 
+        return HttpResponse(json.dumps(context), content_type="application/json")
+
+# Retrieves cache mode (Write back, ...) from a command line
+def cache_mode(mode):
+
+    if mode == '1':
+        return "Write Back"
+    if mode == '3':
+        return "Write Through"
+    if mode == '2':
+        return "Read Only"
+    return "Unknown"
+
+# Stylize the state and gives it color based on NORMAL/DEGRADED/FAILED
+def styled_state(state):
+
+    if state == "normal":
+        return '<span class="text-success">NORMAL</span>'
+    elif state == "degraded":
+        return '<span class="text-warning">DEGRADED</span>'
+    elif state == "failed":
+        return '<span class="text-danger">FAILED</span>'
+    else:
+        return '<span class"text-danger">UNKNOWN (' + state + ')</span>'
+
+def caches():
+    call("./caches.sh")
+    f = open(caches.txt, 'r')
+    c=[]
+    for line in f:
+        c.append(line.rstrip("\n"))
+    return c
+
+# Retrieves cache stat from CMD
+def cache_config(request, cache_name):
+    config = f2d("/proc/rapidstor/" + cache_name + "/config")
+    return HttpResponse(json.dumps({
+        'main_disk_name': config['src_name'],
+        'main_disk_size': config['src_size'],
+        'cache_disk_name': config['ssd_name'],
+        'cache_size': config['ssd_size'],
+        'cache_type': cache_mode(config['mode']),
+        'cache_state': styled_state(config['state']),
+        'block_size': config['block_size']
+    }), content_type="application/json")
+
+# Main function for retrieving, chooses from three functions below
 def instant_stat(request):
 
     if request.method == u'GET':
-        context = dummy_retrieve_stats("cache1")
+        context = retrieve_stats("cache1")
         return HttpResponse(json.dumps(context), content_type="application/json")
 
-
+# Creates dummy cache stats for testing purposes
 def dummy_retrieve_stats(cache_name):
 
     context = {
@@ -40,6 +110,7 @@ def dummy_retrieve_stats(cache_name):
     }
     return context
 
+# Retrieves cache stats from CMD
 def retrieve_stats(cache_name):
     dic = f2d("/proc/rapidstor/"+cache_name+"/stats")
     context = {
@@ -54,12 +125,13 @@ def retrieve_stats(cache_name):
     }
     return context
 
+# Retrieves cache stats from DB
 def retrieve_db(request):
     l = Log.objects.latest('id');
     context = l.__dict
     return context
 
-
+# Changes a stat file into a dictionary
 def f2d(file_name):
     f = open(file_name, 'r')
     d = dict()
