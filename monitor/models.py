8d2b2580075204d4ad502eb8591208d2b09b2339
sma1992 <samafaky@gmail.com>
2015-05-26 11:04:47 +0430

    Integrated

--- monitor/models.py
+++ monitor/models.py
@@ -13,6 +13,8 @@ class Log(models.Model):
     write_hit_rate = models.FloatField(default=0)
     throughput_write = models.FloatField(default=0)
     throughput_read = models.FloatField(default=0)
+    mean_write_time = models.FloatField(default=0)
+    mean_read_time = models.FloatField(default=0)
     read_requests = models.IntegerField(default=0)
     write_requests = models.IntegerField(default=0)
     cur_write_time = models.FloatField(default=0)
