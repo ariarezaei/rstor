6790868900944b53650c68c4da084fe31ea35289
sma1992 <samafaky@gmail.com>
2015-05-26 11:19:58 +0430

    Integrated

--- Disks.sh
+++ Disks.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 rm -f Disks.txt
-echo SDA >> Disks.txt
 fdisk -l | grep Disk | grep sectors | while read line; do
 	s1=$(echo $line | cut -d' ' -f2 | cut -d: -f1)
 	s2=$(echo $line | cut -d' ' -f3)
