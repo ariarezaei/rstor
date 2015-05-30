8ca7bdf7134777c0ed381bd34525898f93abc7da
ariarezaei <aria.rezaii@gmail.com>
2015-05-30 19:33:45 +0430

    Create and Edit pages

--- manageCaches/forms.py
+++ manageCaches/forms.py
@@ -27,34 +27,35 @@ class CacheForm(forms.Form):
     error_css_class = 'text-danger'
 
     name = forms.CharField(label='Cache Name', max_length=30, required=True,
-                           error_messages={"required": "Please enter the cache name."})
+                           error_messages={"required": "Please enter the cache name."},
+                           widget=form.TextInput(attrs={'class': 'form-control'}))
     mode = forms.ChoiceField(label="Mode",
         choices=MODE_CHOICES,
         initial='',
-        widget=forms.Select(),
+        widget=forms.Select(attrs={'class': 'form-control'}),
         required=False
     )
     block_size = forms.ChoiceField(label="Block Size",
         choices=BODE_CHOICES,
         initial='',
-        widget=forms.Select(),
+        widget=forms.Select(attrs={'class': 'form-control'}),
         required=False
     )
     eviction = forms.ChoiceField(label="Eviction",
         choices=PODE_CHOICES,
         initial='',
-        widget=forms.Select(),
+        widget=forms.Select(attrs={'class': 'form-control'}),
         required=False
     )
     ssd = forms.ChoiceField(label="SSD",
         choices=disks(),
         initial='',
-        widget=forms.Select(),
+        widget=forms.Select(attrs={'class': 'form-control'}),
         required=True
     )
     hdd = forms.ChoiceField(label="SRC",
         choices=disks(),
         initial='',
-        widget=forms.Select(),
+        widget=forms.Select(attrs={'class': 'form-control'}),
         required=True
     )
