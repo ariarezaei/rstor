fbc897e1b003c70d0c22a82c1a6733dec1c2c213
ariarezaei <aria.rezaii@gmail.com>
2015-05-31 05:10:03 +0430

    Create and Edit pages

--- manageCaches/views.py
+++ manageCaches/views.py
@@ -197,7 +197,7 @@ def parse_status_message(file):
     res = ""
     with open(file, 'r') as status_file:
         for line in status_file:
-            if ':' in line:
+            if ':' in line or '=' in line:
                 continue
             else:
                 res += line.replace('\n', '<br>')
